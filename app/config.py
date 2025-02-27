a2291fbb3716f2b24b888773ee9b924abc5bb251
cibd <armen.krut@bk.ru>
2025-02-27 14:35:39 +0500

    for server

--- app/config.py
+++ app/config.py
@@ -1,13 +1,18 @@
-# 1. config.py
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:admin12345@localhost/Client_Inf_Base"
-    SECRET_KEY: str = "dsfhsdjkfhsdghfuysdgfgs7dtfsdfgsdfg9s8fg9sfg"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    DATABASE_URL: str
+    SECRET_KEY: SecretStr
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
-    class Config:
-        env_file = ".env"
+    # Указываем путь к файлу .env и настройки загрузки
+    model_config = SettingsConfigDict(
+        env_file="/home/cibd/ClientInfBase/.env",  # Абсолютный путь к файлу .env
+        env_file_encoding="utf-8",  # Кодировка файла
+        extra="ignore",  # Игнорировать лишние переменные в .env
+    )
 
+# Создаем экземпляр настроек
 settings = Settings()
